d69344a6f063edcbdbc2923571f01784d4b5c3a6
Arjunkuls <arjunkuls@gmail.com>
2022-07-15 17:10:10 +0530

    Storing Time as a 100th part of a second in time_passed Logging time_passed:fps to time.txt
    TODO: Calculate delta pos of feet - pos of ears per unit second

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ mpDraw = mp.solutions.drawing_utils
 mppose = mp.solutions.pose
 pose = mppose.Pose()
 
+# Time passed
+# Stored as seconds value * 100
+time_passed = 0
+start_time = time.time()
+
 positions = []
 counter = 0
+# cap = cv2.VideoCapture(0)
 cap = cv2.VideoCapture("yeet.mp4")
 ptime = 0
 while(True):
-      
     # Capture the video frame
     # by frame
     ret, frame = cap.read()
@@ -48,16 +53,26 @@ while(True):
   
     # Calculate FPS
     ctime = time.time()
-    fps = 1/(ctime-ptime)
+    fps = int(1/(ctime-ptime))
     ptime = ctime
 
-    # Display FPS
-    cv2.putText(frame, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+    # Time Logic
+    time_passed = int((time.time() - start_time)*100)
+    # Trigger every 5 seconds
+    if time_passed % 5 == 0:
+        # Write time:FPS to file
+        with open('time.txt', 'a') as f:
+            f.write(str(time_passed) + ":" + str(fps))
+            f.write("\n")
+
+    
+
+    # Display Time Passed
+    cv2.putText(frame, str(int(time_passed)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
 
     # Display the resulting frame
     cv2.imshow('frame', frame)
-      
-    
+
     # the 'q' button is set as the
     # quitting button you may use any
     # desired button of your choice
